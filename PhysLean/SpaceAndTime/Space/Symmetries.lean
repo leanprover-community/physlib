4115232405627bacfec08cbf99b99bfe4a93c330
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2026-02-25 09:13:45 -0800

    Update Symmetries.lean

--- PhysLean/SpaceAndTime/Space/Symmetries.lean
+++ PhysLean/SpaceAndTime/Space/Symmetries.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Tooby-Smith, Pietro Monticone
+Authors: Pietro Monticone
 -/
 import PhysLean.SpaceAndTime.Space.Basic
 /-!
