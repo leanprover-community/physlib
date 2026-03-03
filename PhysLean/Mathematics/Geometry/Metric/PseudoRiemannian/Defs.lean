9283f59e1741ceb4a04fb71e58cb2f0f255dff04
Matteo CIpollina <matteo.cipollina@yahoo.it>
2026-03-03 11:57:35 +0100

    fix

--- PhysLean/Mathematics/Geometry/Metric/PseudoRiemannian/Defs.lean
+++ PhysLean/Mathematics/Geometry/Metric/PseudoRiemannian/Defs.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Matteo Cipollina. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina
 -/
-import PhysLean.Mathematics.Geometry.Metric.PseudoRiemannian.Defs
 import Mathlib.Geometry.Manifold.VectorBundle.Riemannian
 import Mathlib.Geometry.Manifold.VectorBundle.Tangent
 import Mathlib.LinearAlgebra.BilinearForm.Properties
