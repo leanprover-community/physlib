294d01f74da11983abf86964a519f9cf8050aaad
Curly-Howard-Chungus Correspondence | Lamport-Cabot-Codd-Backus-Naur Form <benghia1st@gmail.com>
2026-03-09 04:58:05 +0000

    chore: add copyright and license information to PosReal.lean

--- PhysLean/Meta/Types/PosReal.lean
+++ PhysLean/Meta/Types/PosReal.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Trong-Nghia Be. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Trong-Nghia Be
+-/
 import Mathlib.Data.Real.Basic
 
 /-- The type of strictly positive real numbers. -/
